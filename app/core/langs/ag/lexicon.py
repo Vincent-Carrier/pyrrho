04ed379acae0d4b50b9eaf176105ae4304509ee7
Vincent Carrier <vincentcarrier15@gmail.com>
2023-04-29 12:25:26 -0400

    Simplify LSJ implementation

--- app/core/langs/ag/lexicon.py
+++ app/core/langs/ag/lexicon.py
@@ -1,29 +1,16 @@
+import csv
 import re
 import shelve
 from itertools import islice
-from pathlib import Path
-
-from lxml import etree
-
-from app.core.lexicon import LexiconEntry
 
 TRAILING_REGEX = re.compile(r"(\s|\.|,|;)+$")
 
 
 async def seed_lsj():
     print("Seeding LSJ...")
-    for p in Path("data/ag/lsj").glob("*.xml"):
-        print(f"Processing {p}...")
-        root = etree.parse(p.as_posix()).getroot()
-        with shelve.open("data/ag/lsj.db") as db:
-            for entry in root.findall(".//entryFree"):
-                lemma = str(entry.attrib["key"])
-                definitions = [
-                    re.sub(TRAILING_REGEX, "", e.text)
-                    for e in entry.findall(".//tr")
-                    if e.text
-                ]
-                if len(definitions) > 0:
-                    db[lemma] = LexiconEntry(lemma, definitions)
+    with open ("data/ag/LSJ_shortdefs.tsv") as f:
+        with shelve.open("data/ag/lsj") as db:
+            for [lemma, definition] in csv.reader(f, dialect="excel-tab"):
+                db[lemma] = definition
 
-        print(f"Sample: {list(islice(db.items(), 5))}")
+            print(f"Sample: {list(islice(db.items(), 5))}")
--- app/core/lexicon.py
+++
@@ -1,6 +0,0 @@
-from typing import NamedTuple
-
-
-class LexiconEntry(NamedTuple):
-    lemma: str
-    definitions: list[str]
